a106fc8a26a3c704cedb13c9063966513227b6a6
“Yiqi <2640204l@student.gla.ac.uk>
2022-02-05 23:43:40 +0000

    chapter 6

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
@@ -7,16 +8,22 @@ class Category(models.Model):
     # Specify max_length to provide a maximum number of characters that a CharField field can store.
     name = models.CharField(max_length=128, unique=True)
 
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
     # For each field, you can specify the unique attribute. 
     # If set to True, the given field’s value must be unique 
     # throughout the underlying database table that is mapped to the associated model.
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name) 
+        super(Category, self).save(*args, **kwargs)
 
     # typo within the admin interface (Categorys, not Categories). This typo can be fixed
     class Meta:
         verbose_name_plural = 'Categories'
 
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
--- rango/urls.py
+++ rango/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # first parameter is the string to match. empty string '', only match if there is nothing after
     # second parameter tells Django what view to call if matched
     # third and optional parameter is called name
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',views.show_category, name='show_category'),
 ]
